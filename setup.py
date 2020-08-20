c8d60a5ba779cb157d774f20a73d6a6538c86cf4
hibiki <20015683+ReKTDevlol@users.noreply.github.com>
2020-08-20 17:07:56 +0800

    incr ver

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nekobot",
-    version="1.1",
+    version="1.2",
     author="hibikidesu",
     description="NekoBot API Python Wrapper",
     keywords="nekobot api discord",
